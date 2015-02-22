3199b2682d9e7a06ea9f3754d9fd847f323ad94b
scotchfield <swrittenb@gmail.com>
2015-02-22 11:03:44 -0500

    Handle render callbacks in example game, some text adds

--- examples/simple-game/game.js
+++ examples/simple-game/game.js
@@ -1,18 +1,52 @@
 var game = (function () {
-    var canvas, state,
+    var canvas, state, renderCb = [];
+
+    renderCb.removeCb = function(cb) {
+        for (var i = this.length; i >= 0; i -= 1) {
+            if (this[i] === cb) {
+                this.splice(i, 1);
+            }
+        }
+
+        return this;
+    };
+
+    var renderTitle = function () {
+        rl.clear()
+            .style('#ffffff')
+            .write('helo', 1, 1)
+            .style('#cccccc')
+            .write('press a key to continue', 1, 2);
+    },
+    renderGame = function () {
+        rl.clear();
+    },
+    render = function () {
+        renderCb.forEach(
+            function (cb) {
+                cb();
+            }
+        );
+    },
 
     keydownMap = function keydown(e) {
         console.log(e);
-    };
+    },
     keydownTitle = function keydown(e) {
         rl.unregisterKeydown(keydown)
             .registerKeydown(keydownMap);
+        renderCb.removeCb(renderTitle)
+            .push(renderGame);
         state = 'map';
-    };
+        render();
+    },
 
     canvas = rl.canvas();
     document.getElementById('game_canvas').appendChild(canvas);
 
-    state = 'title';
     rl.registerKeydown(keydownTitle);
+
+    state = 'title';
+    renderCb.push(renderTitle);
+    render();
 }());
