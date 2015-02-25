c6cd168071038d1f31d9a4e0dec31260ce8ec2a3
scotchfield <swrittenb@gmail.com>
2015-02-24 23:04:45 -0500

    Add movement code to simple game, refactor setup

--- examples/simple-game/game.js
+++ examples/simple-game/game.js
@@ -1,5 +1,6 @@
 var game = (function () {
-    var canvas, state, renderCb = [];
+    var canvas, state, renderCb = [],
+        player, width = 40, height = 25;
 
     renderCb.removeCb = function(cb) {
         for (var i = this.length; i >= 0; i -= 1) {
@@ -11,7 +12,18 @@ var game = (function () {
         return this;
     };
 
-    var renderTitle = function () {
+    var resetPlayer = function () {
+        player = {
+            x: 0, y: 0, c: '@', style: '#ffffff',
+        };
+    },
+
+    setup = function () {
+        state = 'map';
+        resetPlayer();
+    },
+
+    renderTitle = function () {
         rl.clear()
             .style('#ffffff')
             .write('helo', 1, 1)
@@ -19,7 +31,9 @@ var game = (function () {
             .write('press a key to continue', 1, 2);
     },
     renderGame = function () {
-        rl.clear();
+        rl.clear()
+            .style(player.style)
+            .write(player.c, player.x, player.y);
     },
     render = function () {
         renderCb.forEach(
@@ -30,21 +44,36 @@ var game = (function () {
     },
 
     keydownMap = function keydown(e) {
-        console.log(e);
+        if (e.keyCode === 68) {
+            player.x += 1;
+        } else if (e.keyCode === 65) {
+            player.x -= 1;
+        } else if (e.keyCode === 83) {
+            player.y += 1;
+        } else if (e.keyCode === 87) {
+            player.y -= 1;
+        }
+        player.x = player.x < 0 ? 0 : player.x;
+        player.x = player.x >= width ? width - 1 : player.x;
+        player.y = player.y < 0 ? 0 : player.y;
+        player.y = player.y >= height ? height - 1 : player.y;
+        render();
     },
     keydownTitle = function keydown(e) {
         rl.unregisterKeydown(keydown)
             .registerKeydown(keydownMap);
         renderCb.removeCb(renderTitle)
             .push(renderGame);
-        state = 'map';
+        setup();
         render();
     };
 
     var options = {
         font: '12pt monospace',
         tileWidth: 16,
-        tileHeight: 16
+        tileHeight: 16,
+        width: width,
+        height: height,
     };
 
     rl.create('game_canvas', options)
